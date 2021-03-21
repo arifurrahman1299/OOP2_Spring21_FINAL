9e7b2e8e7f9d598540e43c1a8dd63c3e488aca41
arifurrahman1299 <75237862+arifurrahman1299@users.noreply.github.com>
2021-03-21 18:46:01 +0600

    Add files via upload

---
+++ FinalLabTask2/FinalLabTask2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalLabTask2
+{
+    class Program
+    {
+        Account a1 = new Account("Arifur", "1111", 15000.0f);
+        Account a2 = new Account("Rahman", "1112", 10000.0f);
+
+        Transaction t = new Transaction(a1, a2, 100);
+
+
+    }
+}
