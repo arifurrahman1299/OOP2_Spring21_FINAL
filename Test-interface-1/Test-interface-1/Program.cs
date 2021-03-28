40f264fc48ee2e43f25b052733b6baacc63a4a43
arifurrahman1299 <75237862+arifurrahman1299@users.noreply.github.com>
2021-03-28 13:24:14 +0600

    Add files via upload

---
+++ Test-interface-1/Test-interface-1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_interface_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator cal = new Calculator();
+            int sum = cal.sum(50, 100);
+            Console.WriteLine(sum);
+
+            Scientific sc = new Scientific();
+            int res = sc.XtoY(6, 3);
+            Console.WriteLine(res);
+
+        }
+    }
+}
