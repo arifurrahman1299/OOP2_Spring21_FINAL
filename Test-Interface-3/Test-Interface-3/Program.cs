3ba319bd990e863b421ac5147b2ad97c96690a3f
arifurrahman1299 <75237862+arifurrahman1299@users.noreply.github.com>
2021-03-30 18:56:52 +0600

    Add files via upload

---
+++ Test-Interface-3/Test-Interface-3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test_Interface_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicFile m1 = new MusicFile("Believer", "Imagine Dragon", 2017, 204);
+            MusicFile m2 = new MusicFile("Boulevard Of Broken Dreams", "Green Day", 2004, 260);
+            Phone phone = new MusicPlayer("Music Player", m1);
+            phone.addMusic(m1, m2);
+            phone.play(true);
+            phone.playNext();
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Phone radio = new RadioPlayer("Radio Player", 89.5);
+            radio.play(true);
+            radio.playNext();
+        }
+    }
+}
