bf5025d56c26f373df82cd095580c5ee9b04a860
arifurrahman1299 <75237862+arifurrahman1299@users.noreply.github.com>
2021-04-11 19:56:59 +0600

    Add files via upload

---
+++ DataInsertion_DB/DataInsertion_DB/Form1.Designer.cs
@@ -0,0 +1,112 @@
+﻿
+namespace DataInsertion_DB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbCCode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbCName = new System.Windows.Forms.TextBox();
+            this.btnAddCourse = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(34, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(154, 35);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Couse Code";
+            // 
+            // tbCCode
+            // 
+            this.tbCCode.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCCode.Location = new System.Drawing.Point(214, 35);
+            this.tbCCode.Name = "tbCCode";
+            this.tbCCode.Size = new System.Drawing.Size(351, 38);
+            this.tbCCode.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(160, 35);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Couse Name";
+            // 
+            // tbCName
+            // 
+            this.tbCName.Font = new System.Drawing.Font("Arial Narrow", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbCName.Location = new System.Drawing.Point(214, 105);
+            this.tbCName.Name = "tbCName";
+            this.tbCName.Size = new System.Drawing.Size(351, 38);
+            this.tbCName.TabIndex = 3;
+            // 
+            // btnAddCourse
+            // 
+            this.btnAddCourse.Font = new System.Drawing.Font("Arial Narrow", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAddCourse.Location = new System.Drawing.Point(409, 171);
+            this.btnAddCourse.Name = "btnAddCourse";
+            this.btnAddCourse.Size = new System.Drawing.Size(156, 52);
+            this.btnAddCourse.TabIndex = 4;
+            this.btnAddCourse.Text = "Add Course";
+            this.btnAddCourse.UseVisualStyleBackColor = true;
+            this.btnAddCourse.Click += new System.EventHandler(this.AddCourse);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnAddCourse);
+            this.Controls.Add(this.tbCName);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbCCode);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbCCode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbCName;
+        private System.Windows.Forms.Button btnAddCourse;
+    }
+}
+
---
+++ DataInsertion_DB/DataInsertion_DB/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace DataInsertion_DB
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void AddCourse(object sender, EventArgs e)
+        {
+            string c_code = tbCCode.Text;
+            string c_name = tbCName.Text;
+
+            string connString = @"server=desktop-cb0xx\sqlexpress;Database=OOP_Final;Integrated Security=true";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            string query = string.Format("insert into courses value('{0}','{1}')", c_code, c_name);
+            SqlCommand cmd = new SqlCommand(query, conn);
+            int r = cmd.ExecuteNonQuery();
+            if(r>0)
+            {
+                MessageBox.Show("Data Inserted");
+            }
+            else
+            {
+                MessageBox.Show("Data Didn't Inserted");
+            }
+            conn.Close();
+        }
+    }
+}
